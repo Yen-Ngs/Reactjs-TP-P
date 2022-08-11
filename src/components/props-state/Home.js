cf7dfba59fe44ee4aa9626e8cc579570fc16d4b7
Yen-Ngs <75205951+Yen-Ngs@users.noreply.github.com>
2022-08-11 13:15:39 +0700

    update 1

--- src/components/props-state/Home.js
+++ src/components/props-state/Home.js
@@ -13,23 +13,12 @@ export default function Home() {
       <header className="App-header">
         <Countdown seconds={30} />
 
-        <ColorBox color="purple" />
-        <ColorBox color="green" />
+        <ColorBox color="#A0BEF8" />
+        <ColorBox color="#b9f3f9" />
       </header>
     </div>
     </div>
-    // <>
-    // <Navbar bg="dark" variant="dark">
-    //   <Container>
-    //     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    //     <Nav className="me-auto">
-    //       <Nav.Link href="#home" to="Home">Home</Nav.Link>
-    //       <Nav.Link href="arc" to="">AdvoidingRC</Nav.Link>
-    //       <Nav.Link href="#values"></Nav.Link>
-    //     </Nav>
-    //   </Container>
-    // </Navbar>
-    // </>
+    
       );
 
 
--- src/components/var/Var.js
+++ src/components/var/Var.js
@@ -4,8 +4,6 @@ import React from 'react'
 
 export default function Var() {
 
-    var greeter = "hey hi";
-    console.log(greeter)
   return (
     <div>Var</div>
   )
